1c165929ba046a782626fa683fe331584232dd41
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-07-07 15:05:57 +0000

    app js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Contacts from './Contacts';
+import ContactsForm from './Form';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      contacts: [
+        { name: "Caleb", phonenumber: "0291021912", location: "Tema" },
+        { name: "Jerry", phonenumber: "0291021912", location: "Kumasi" },
+        { name: "Andy", phonenumber: "0291021912", location: " Teshi" },
+      ]
+    }
+  }
+
+  handleAddContact = (newContact) => {
+    this.setState({
+      contacts: [...this.state.contacts, newContact]
+    })
+  }
+
+  render() {
+    const contacts = this.state.contacts.map((contact, index) => {
+      return (
+        <Contacts contact={contact} key={index} />
+      )
+    })
+    return (
+      <div  className="completeContact" >
+        <ContactsForm addContact={this.handleAddContact} />
+        {contacts}
+
+      </div>
+    );
+  }
 }
 
 export default App;
