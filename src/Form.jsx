7a63309776f831aa413476fb4cdfd8c721c6caca
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-07-07 15:06:42 +0000

    forms files

---
+++ src/Form.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      phoneNumber: "",
+      location: "",
+    };
+  }
+
+  handleChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.addContact(this.state);
+
+    this.setState({
+      name: "",
+      phoneNumber: "",
+      location: "",
+    });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h1>Create Contact</h1>
+        <div>
+          <label htmlFor="name">Name: </label>
+          <input
+            className="inputField"
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="phoneNumber">Phone Number: </label>
+          <input
+            className="inputField"
+            type="number"
+            name="phoneNumber"
+            value={this.state.phoneNumber}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="locaton">Location: </label>
+          <input
+            className="inputField"
+            type="text"
+            name="location"
+            value={this.state.location}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div>
+          <button className="submit">Add Contact</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Form;
