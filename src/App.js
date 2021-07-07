a0db4ca03e1bbe530eaa3317cf205b8e9c9dcfb3
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-07-07 15:21:58 +0000

    number update

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ class App extends Component {
     super(props)
     this.state = {
       contacts: [
-        { name: "Caleb", phonenumber: "0291021912", location: "Tema" },
-        { name: "Jerry", phonenumber: "022332343", location: "Kumasi" },
-        { name: "Andy", phonenumber: "02932324343", location: " Teshi" },
+        { name: "Caleb", phonenumber: 291021912, location: "Tema" },
+        { name: "Jerry", phonenumber: 323233232332, location: "Kumasi" },
+        { name: "Andy", phonenumber: 2932324343, location: " Teshi" },
       ]
     }
   }
