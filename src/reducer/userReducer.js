a775ba1c8492e7a5c8a04a991b495f2c0d8cd059
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-08-10 12:00:39 +0000

    user reducer.js

---
+++ src/reducer/userReducer.js
@@ -0,0 +1,15 @@
+
+const initialState={contacts: [
+    { name: "Calebnvnvn", phonenumber: 291021912, location: "Tema" },
+    { name: "Jerry", phonenumber: 323233232332, location: "Kumasi" },
+    { name: "Andy", phonenumber: 2932324343, location: " Teshi" },
+  ]
+} 
+const userReducer = (state={initialState},action) => {
+   switch (action.type) {
+       case state:
+           
+          return state
+        }}
+
+export default userReducer;
