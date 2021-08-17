708858579ddf5d8253f032a7d7897aa22ef4fb14
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-08-17 12:06:28 +0000

    user reducer js update

--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,15 +1,17 @@
 
-const initialState={contacts: [
-    { name: "Calebnvnvn", phonenumber: 291021912, location: "Tema" },
-    { name: "Jerry", phonenumber: 323233232332, location: "Kumasi" },
-    { name: "Andy", phonenumber: 2932324343, location: " Teshi" },
-  ]
-} 
-const userReducer = (state={initialState},action) => {
-   switch (action.type) {
-       case state:
-           
-          return state
-        }}
+  
+const initialState = {
+  users: [],
+};
 
-export default userReducer;
+const usersRuducers = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_USER":
+      return { ...state.users, users: [...state.users, action.payload] };
+
+    default:
+      return state;
+  }
+};
+
+export default usersRuducers;
