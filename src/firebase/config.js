233373a8ce475dfe4a21c556307326445c684a5c
kenneth adjei-mensah <kenadjei20@gmail.com>
2021-09-07 13:25:38 +0000

    new config firebase file

---
+++ src/firebase/config.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDayvooHyapiQpCxLEk1NovGspuEC6nbg8",
+  authDomain: "user-76a8d.firebaseapp.com",
+  projectId: "user-76a8d",
+  storageBucket: "user-76a8d.appspot.com",
+  messagingSenderId: "911974025133",
+  appId: "1:911974025133:web:de5264835491a8e009c324",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
